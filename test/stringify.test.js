5863e14d844a4b355392fe557fefa3a964da5476
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-01-02 13:06:29 -0500

    parse/stringify streams support

--- test/stringify.test.js
+++ test/stringify.test.js
@@ -1,6 +1,28 @@
 var assert = require('assert'),
   stringify = require('../').stringify,
-  bson = require('bson');
+  parse = require('../').parse,
+  createStringifyStream = require('../').createStringifyStream,
+  createParseStream = require('../').createParseStream,
+  bson = require('bson'),
+  fs = require('fs'),
+  os = require('os'),
+  es = require('event-stream');
+
+var util = require('util');
+var Readable = require('stream').Readable;
+
+function DocsStream(docs){
+  Readable.call(this, {objectMode: true});
+  this.docs = docs;
+}
+util.inherits(DocsStream, Readable);
+
+DocsStream.prototype._read = function(){
+  this.docs.forEach(function(doc){
+    this.push(doc);
+  }.bind(this));
+  this.push(null);
+};
 
 describe('Stringify', function(){
   it('should work', function(){
@@ -8,5 +30,40 @@ describe('Stringify', function(){
        _id: bson.ObjectID()
      };
      assert.equal(stringify(doc), '{"_id":{"$oid":"'+doc._id.toString()+'"}}');
+  });
+  describe('streaming', function(){
+    var docs = [
+      {
+        _id: bson.ObjectID()
+      }
+    ];
+    var dest = os.tmpdir() + '/ejson-streams-result.json';
+    after(function(done){
+      fs.unlink(dest, function(){
+        done();
+      });
+    });
+    it('should support stringify', function(done){
+      new DocsStream(docs)
+        .pipe(createStringifyStream())
+        .pipe(fs.createWriteStream(dest))
+        .on('finish', function(){
+          fs.readFile(dest, function(err, buf){
+            if(err) return done(err);
+
+            var data = parse(buf);
+            assert.deepEqual(data, docs);
+            done();
+          });
+        });
+    });
+
+    it('should support parse', function(done){
+      fs.createReadStream(dest)
+        .pipe(createParseStream('*'))
+        .on('end', function(){
+          done();
+        });
+    });
   });
 });
