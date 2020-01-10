5557d9aead6620d12f14cc1dd87bb5e02c8d4c3a
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2020-01-10 15:04:23 -0500

    chore: project housekeeping
    - sync .travis.yml
    - remove old .jsfmtrc
    - npm-check-updates
    - prettier on test files
    
    Note the bson@4 bump for testing/semver

--- test/shellmode.test.js
+++ test/shellmode.test.js
@@ -3,25 +3,33 @@ var shellToStrict = require('../lib/modes/shell.js').toStrict;
 var parse = require('../').parse;
 var bson = require('bson');
 
-
 /* eslint new-cap:0 */
 describe('Parse from shell mode', function() {
   it('should work on a simple example with ObjectId', function() {
-    assert.deepEqual(parse('{"_id": ObjectId("53c2b570c15c457669f481f7") }', null, 'shell'), {
-      _id: bson.ObjectID('53c2b570c15c457669f481f7')
-    });
+    assert.deepEqual(
+      parse('{"_id": ObjectId("53c2b570c15c457669f481f7") }', null, 'shell'),
+      {
+        _id: bson.ObjectID('53c2b570c15c457669f481f7')
+      }
+    );
   });
 
   it('should work without field name quotes', function() {
-    assert.deepEqual(parse('{_id: ObjectId("53c2b570c15c457669f481f7") }', null, 'shell'), {
-      _id: bson.ObjectID('53c2b570c15c457669f481f7')
-    });
+    assert.deepEqual(
+      parse('{_id: ObjectId("53c2b570c15c457669f481f7") }', null, 'shell'),
+      {
+        _id: bson.ObjectID('53c2b570c15c457669f481f7')
+      }
+    );
   });
 
   it('should work with single instead of double quotes', function() {
-    assert.deepEqual(parse('{\'_id\': ObjectId(\'53c2b570c15c457669f481f7\') }', null, 'shell'), {
-      _id: bson.ObjectID('53c2b570c15c457669f481f7')
-    });
+    assert.deepEqual(
+      parse("{'_id': ObjectId('53c2b570c15c457669f481f7') }", null, 'shell'),
+      {
+        _id: bson.ObjectID('53c2b570c15c457669f481f7')
+      }
+    );
   });
 });
 
@@ -32,15 +40,25 @@ describe('Shell mode -> Strict mode', function() {
   });
 
   it('should replace Date', function() {
-    var s = shellToStrict('{ "created_at": ISODate("2014-01-01T00:00:00.000Z") }');
-    assert.equal(s, '{ "created_at": { "$date": "2014-01-01T00:00:00.000Z" } }');
+    var s = shellToStrict(
+      '{ "created_at": ISODate("2014-01-01T00:00:00.000Z") }'
+    );
+    assert.equal(
+      s,
+      '{ "created_at": { "$date": "2014-01-01T00:00:00.000Z" } }'
+    );
   });
 
   it('should replace multiple dates in one line', function() {
-    var s = shellToStrict('{ "start": ISODate("2014-01-01T00:00:00.000Z"), "end": '
-      + 'ISODate("2015-01-01T00:00:00.000Z") }');
-    assert.equal(s, '{ "start": { "$date": "2014-01-01T00:00:00.000Z" }, "end": '
-      + '{ "$date": "2015-01-01T00:00:00.000Z" } }');
+    var s = shellToStrict(
+      '{ "start": ISODate("2014-01-01T00:00:00.000Z"), "end": ' +
+        'ISODate("2015-01-01T00:00:00.000Z") }'
+    );
+    assert.equal(
+      s,
+      '{ "start": { "$date": "2014-01-01T00:00:00.000Z" }, "end": ' +
+        '{ "$date": "2015-01-01T00:00:00.000Z" } }'
+    );
   });
 
   it('should replace Timestamp', function() {
@@ -59,15 +77,25 @@ describe('Shell mode -> Strict mode', function() {
   });
 
   it('should not confuse URLs with RegExp', function() {
-    var s = shellToStrict('{ url: "https://www.google.com/accounts/cd/id?abc=123" }');
-    assert.equal(s, '{ "url": "https://www.google.com/accounts/cd/id?abc=123" }');
+    var s = shellToStrict(
+      '{ url: "https://www.google.com/accounts/cd/id?abc=123" }'
+    );
+    assert.equal(
+      s,
+      '{ "url": "https://www.google.com/accounts/cd/id?abc=123" }'
+    );
   });
 
   it('should handle RegExp with embedded URLs', function() {
-    var s = shellToStrict('{ "url": /(?:^|\W)href="http:\/\/www\.google\.com\/index'
-      + 'es\/12345678\/0987654321a\/"/i }');
-    assert.equal(s, '{ "url": { "$regex": "(?:^|\W)href="http:\/\/www\.google\.com\/index'
-      + 'es\/12345678\/0987654321a\/"", "$options": "i" } }');
+    var s = shellToStrict(
+      '{ "url": /(?:^|W)href="http://www.google.com/index' +
+        'es/12345678/0987654321a/"/i }'
+    );
+    assert.equal(
+      s,
+      '{ "url": { "$regex": "(?:^|W)href="http://www.google.com/index' +
+        'es/12345678/0987654321a/"", "$options": "i" } }'
+    );
   });
 
   it('should not confuse string paths with RegExp', function() {
@@ -77,7 +105,10 @@ describe('Shell mode -> Strict mode', function() {
 
   it('should replace BinData', function() {
     var s = shellToStrict('{ "bin": BinData(0,"SGVsbG8gV29ybGQ=") }');
-    assert.equal(s, '{ "bin": { "$binary": "SGVsbG8gV29ybGQ=", "$type": "0" } }');
+    assert.equal(
+      s,
+      '{ "bin": { "$binary": "SGVsbG8gV29ybGQ=", "$type": "0" } }'
+    );
   });
 
   it('should replace NumberLong without quotes', function() {
